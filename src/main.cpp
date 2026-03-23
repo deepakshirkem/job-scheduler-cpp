403433b0e3f8848d672a323cac26f66b742b979b
deepakshirkem <deepakshirke509@gmail.com>
2026-03-24 01:12:00 +0530

    Fix typo in available commands message

--- src/main.cpp
+++ src/main.cpp
@@ -90,7 +90,7 @@ int main()
         }
         else
         {
-            return "Unknown command: " + cmd + "\nAvailable: STATUS, SUBMIT, WATCH <cmd> , SHUTDOWN\n";
+            return "Unknown command: " + cmd + "\nAvailable: STATUS, SUBMITED, WATCH <cmd> , SHUTDOWN\n";
         }
     });
 
